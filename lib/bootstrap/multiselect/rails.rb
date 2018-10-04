643ae0c1e8dbfa46876bf59e032b703fa0763bf6
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2018-10-04 17:10:02 +0900

    initial commit

---
+++ lib/bootstrap/multiselect/rails.rb
@@ -0,0 +1,10 @@
+require "bootstrap/multiselect/rails/version"
+
+module Bootstrap
+  module Multiselect
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/bootstrap/multiselect/rails/version.rb
@@ -0,0 +1,7 @@
+module Bootstrap
+  module Multiselect
+    module Rails
+      VERSION = "0.9.15"
+    end
+  end
+end
